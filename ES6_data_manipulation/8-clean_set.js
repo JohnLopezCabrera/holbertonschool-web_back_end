773d481b4a4a7b59a20fa3bb2d9905db4b651228
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-07-22 16:33:54 -0400

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,14 @@
-// 8-clean_set.js
-
 export default function cleanSet(set, startString) {
-  // Filter the set to get values that start with startString
-  // Remove startString from the beginning of each value
-  // Join the results with '-' separator
+  if (typeof startString !== 'string') {
+    throw new TypeError('startString must be a string');
+  }
+
+  if (!startString) {
+    return '';
+  }
+
   return [...set]
-    .filter((value) => value.startsWith(startString))
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
 }
