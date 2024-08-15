a387e397dc3771562a10213745f418d007660b19
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-08-15 17:03:15 -0400

    second commit

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function concatenate two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and return the result.
+
+    Args:
+        str1 (str): The 1st string to join.
+        str2 (str): The 2nd string to join.
+
+    Returns:
+        str: The resulting combined string.
+    """
+    return str1 + str2
