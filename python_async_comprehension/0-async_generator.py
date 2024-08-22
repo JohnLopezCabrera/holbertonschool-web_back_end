6850782c6ec786faa1b418b9078c74ebfbe33ea5
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-08-21 22:50:48 -0400

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""Module for asynchronous generator."""
+"""#Module for asynchronous generator."""
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]: #type: ignore
-    """Asynchronously takes a float between 0 and 10.
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Asynchronously generates 10 random floats between 0 and 10.
 
     This function pauses for 1 second between each to simulate
-    asynchronous i/o. It creates 10 random numbers in total.
+    asynchronous I/O operations. It creates 10 random numbers in total.
     """
     for _ in range(10):
         await asyncio.sleep(1)  # Pause execution for 1 second
