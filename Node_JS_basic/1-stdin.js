42e339ca610343186c22036f2574c03f4f33d4e3
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-09-17 17:31:44 -0400

    l

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (data) => {
-    const input = data.toString().trim();  // Trim to remove unwanted characters
-    console.log(`Your name is: ${input}`);
-    process.stdout.write("This important software is now closing\n");
-    process.exit();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit(0);
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
